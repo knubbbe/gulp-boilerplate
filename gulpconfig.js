a2ffe93c17d13270eb983cb1219fa25f77415969
knubbbe <knubbbe@gmail.com>
2016-05-13 09:35:37 +0200

    first commit

---
+++ gulpconfig.js
@@ -0,0 +1,76 @@
+var path = require('path');
+
+// Browser definitions for autoprefixer
+var autoprefixer_options = [
+'last 3 versions',
+'ie >= 9',
+'ios >= 7',
+'android >= 4.4'
+];
+
+var paths = {
+	project: {
+		base: '/home/knubbe/www/westart/project/html/',
+		proxy: 'project.westart.se'
+		// proxy: 'sub.project.westart.se'
+	},
+	script: {
+		src: [
+		'/home/knubbe/www/westart/project/html/content/themes/theme/js/scripts.js',
+		'/home/knubbe/www/westart/project/html/content/themes/theme/js/js.cookie.js',
+		'/home/knubbe/www/westart/project/html/content/themes/theme/js/wNumb.js',
+		'/home/knubbe/www/westart/project/html/content/themes/theme/js/jquery.fancybox.js',
+		'!/home/knubbe/www/westart/project/html/content/themes/theme/js/**/*.min.js'
+		],
+		dest: '/home/knubbe/www/westart/project/html/content/themes/theme/js/min',
+		base: '/home/knubbe/www/westart/project/html/content/themes/theme/js/'
+	},
+	style: {
+		src:  [
+			'/home/knubbe/www/westart/project/html/content/themes/theme/scss/**/*.scss',
+			'!/home/knubbe/www/westart/project/html/content/themes/theme/scss/font-awesome/font-awesome.scss'
+		],
+		// dest: '/home/knubbe/www/westart/project/html/content/themes/theme/',
+		// base: '/home/knubbe/www/westart/project/html/content/themes/theme/scss'
+		folders: [
+			'/home/knubbe/www/westart/project/html/content/themes/theme/'
+		]
+	}
+};
+
+var options = {
+	script: {},
+	style: {
+		outputStyle: 'expanded' //  nested, expanded, compact, compressed
+	},
+	eslint: {
+		quiet: true,
+		configFile: '/home/knubbe/Dropbox/.eslintrc'
+	}
+};
+
+var notifications = {
+	script: {
+		// message: "Minified: <%= file.relative %>"
+		error: {
+			title: 'JS Error:',
+			message: '<%= error.message %>',
+			icon: path.join(__dirname, 'icons/nodejs.png')
+		}
+	},
+	style: {
+		message: "Compile: <%= file.relative %>",
+		error: {
+			title: 'SCSS Error:',
+			message: '<%= error.message %>',
+			icon: path.join(__dirname, 'icons/sass.png')
+		}
+	}
+};
+
+module.exports = {
+	autoprefixer_options: autoprefixer_options,
+	paths:                paths,
+	options:              options,
+	notifications:        notifications
+};
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp         = require('gulp');
+var uglify       = require('gulp-uglify');
+var eslint       = require('gulp-eslint');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename       = require('gulp-rename');
+var notify       = require('gulp-notify');
+var browserSync  = require('browser-sync').create();
+var merge        = require('merge-stream');
+var gulpconfig   = require('./gulpconfig.js');
+
+gulp.task('compile:js', function(){
+	return gulp
+		.src(gulpconfig.paths.script.src, { base: gulpconfig.paths.script.base })
+		.pipe(eslint(gulpconfig.options.eslint))
+		.pipe(eslint.format())
+		// .pipe(eslint.failOnError())
+		.pipe(uglify(gulpconfig.options.script))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest(gulpconfig.paths.script.dest))
+		.pipe(browserSync.stream());
+		// .pipe(notify(gulpconfig.notifications.script));
+});
+
+gulp.task('compile:scss', function () {
+	var tasks = gulpconfig.paths.style.folders.map(function(folder){
+		return gulp
+			.src(folder + 'scss/*.scss', { base: folder + 'scss' })
+			.pipe(sass(gulpconfig.options.style)).on('error', notify.onError({
+				title: gulpconfig.notifications.style.error.title,
+				message: gulpconfig.notifications.style.error.message,
+				icon: gulpconfig.notifications.style.error.icon
+			}))
+			.pipe(autoprefixer())
+			.pipe(gulp.dest(folder))
+			.pipe(browserSync.stream())
+			// .pipe(notify(gulpconfig.notifications.style));
+	});
+
+	return merge(tasks);
+});
+
+gulp.task('serve', ['compile:js', 'compile:scss'], function() {
+	browserSync.init({
+		open:      false,
+		proxy:     gulpconfig.paths.project.proxy,
+		port:      8080,
+		ghostMode: false,
+		files: [
+			"content/themes/**/*.css"
+		],
+		snippetOptions: {
+			whitelist: ['/wp-admin/admin-ajax.php']
+		}
+	});
+
+	gulp.watch(gulpconfig.paths.script.src, { interval: 500 }, ['compile:js']);
+	gulp.watch(gulpconfig.paths.style.src, { interval: 500 }, ['compile:scss']);
+	gulp.watch([
+		gulpconfig.paths.project.base + "/**/*.php",
+		gulpconfig.paths.project.base + "/content/plugins/**/*.js",
+		gulpconfig.paths.project.base + "/content/themes/**/*.js",
+	], { interval: 500 }, ['browser-reload']);
+});
+
+gulp.task('browser-reload', function () {
+	browserSync.reload();
+});
+
+gulp.task('default', ['serve']);
